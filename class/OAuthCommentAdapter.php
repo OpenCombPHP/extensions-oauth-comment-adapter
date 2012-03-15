553aafd51858f9656de85e4b28a64b4b642ab4b2
kongyuan <anubiskong@gmail.com>
2012-03-15 14:55:40 +0800

    first commit

---
+++ class/OAuthCommentAdapter.php
@@ -0,0 +1,17 @@
+<?php 
+namespace org\opencomb\oauthcommentadapter ;
+
+use org\jecat\framework\lang\aop\AOP;
+use org\opencomb\platform\ext\Extension ;
+
+class OAuthCommentAdapter extends Extension 
+{
+	/**
+	 * 载入扩展
+	 */
+	public function load()
+	{
+		//修改liststate中链接
+		AOP::singleton()->register('org\\opencomb\\oauthcommentadapter\\aspect\\CommentListAspect') ;
+	}
+}
---
+++ class/aspect/CommentListAspect.php
@@ -0,0 +1,34 @@
+<?php
+namespace org\opencomb\oauthcommentadapter\aspect ;
+
+use org\jecat\framework\message\Message;
+use org\jecat\framework\bean\BeanFactory;
+use org\jecat\framework\mvc\model\db\Model;
+use org\jecat\framework\lang\aop\jointpoint\JointPointMethodDefine;
+
+class CommentListAspect
+{
+	/**
+	 * @pointcut
+	 */
+	public function pointcutCreateBeanConfig()
+	{
+		return array(
+			new JointPointMethodDefine('org\\opencomb\\comment\\CommentList','createBeanConfig') ,
+		) ;
+	}
+	
+	/**
+	 * @advice around
+	 * @for pointcutCreateBeanConfig
+	 */
+	private function createBeanConfig()
+	{
+		$arrConfig = aop_call_origin() ;
+		$arrConfig['controllers']['pullComment'] = array(
+						'class'=>'org\opencomb\oauthcommentadapter\PullComment',
+				);
+		return $arrConfig;
+	}
+}
+?>
